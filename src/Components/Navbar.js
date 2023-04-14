23fce1a73a65f39314161bcad53bc32461399fac
Ary0405 <aryansethia2003@gmail.com>
2023-04-15 02:52:09 +0530

    responsive navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import logo from '../Assets/logo_website.png'
+import hamburger_website from '../Assets/hamburger_website.png'
 import '../Styles/Navbar.css'
 
 function Navbar() {
+    const [showNav, setShowNav] = React.useState(false)
     return (
         <div className='navbar_main_container'>
-            <div className='logo_container'>
-                <img src={logo} />
+            <div className='phone_container'>
+                <div className='logo_container'>
+                    <img src={logo} />
+                </div>
+                <img onClick={() => { setShowNav(!showNav) }} className='hamburger' src={hamburger_website} />
+
             </div>
-            <div className='list_signup'>
+            <div className={'list_signup ' + (showNav && 'active')}>
                 <div className='list_container'>
                     <p>Product</p>
                     <p>Resource</p>
